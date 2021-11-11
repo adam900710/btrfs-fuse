626d9c952c036b032fe5fd0175b7407a280d510c
Qu Wenruo <wqu@suse.com>
2021-11-11 12:24:14 +0800

    btrfs-fuse: user proper types.h for inode.h
    [BUG]
    There is a report that on ppc64le, btrfs-fuse failed to build:
    
    /usr/include/asm-generic/int-ll64.h:30:44: error: conflicting types for ‘__s64’; have ‘long long int’
       30 | __extension__ typedef __signed__ long long __s64;
          |                                            ^~~~~
    In file included from /usr/include/asm/types.h:26,
                     from ../compat.h:6,
                     from ../ondisk_format.h:7,
                     from ../accessors.h:8,
                     from ../main.c:7:
    /usr/include/asm-generic/int-l64.h:29:25: note: previous declaration of ‘__s64’ with type ‘__s64’ {aka ‘long int’}
       29 | typedef __signed__ long __s64;
          |                         ^~~~~
    In file included from /usr/include/asm-generic/types.h:7,
                     from ../inode.h:6,
                     from ../main.c:11:
    /usr/include/asm-generic/int-ll64.h:31:42: error: conflicting types for ‘__u64’; have ‘long long unsigned int’
       31 | __extension__ typedef unsigned long long __u64;
          |                                          ^~~~~
    In file included from /usr/include/asm/types.h:26,
                     from ../compat.h:6,
                     from ../ondisk_format.h:7,
                     from ../accessors.h:8,
                     from ../main.c:7:
    /usr/include/asm-generic/int-l64.h:30:23: note: previous declaration of ‘__u64’ with type ‘__u64’ {aka ‘long unsigned int’}
       30 | typedef unsigned long __u64;
          |                       ^~~~~
    
    [CAUSE]
    On ppc64le, it has a different set of definition for s64/u64, this can
    be found from its types.h in arch/powerpc/include/uapi/asm/:
    
      /*
       * This is here because we used to use l64 for 64bit powerpc
       * and we don't want to impact user mode with our change to ll64
       * in the kernel.
       *
       * However, some user programs are fine with this.  They can
       * flag __SANE_USERSPACE_TYPES__ to get int-ll64.h here.
       */
      #if !defined(__SANE_USERSPACE_TYPES__) && defined(__powerpc64__) && !defined(__KERNEL__)
      # include <asm-generic/int-l64.h>
      #else
      # include <asm-generic/int-ll64.h>
      #endif
    
    While in "inode.h" it's skips <asm/types.h> but include
    <asm-generic/types.h> directly, causing different definition for u64/s64
    and causing above problem.
    
    [FIX]
    Fix it by including <asm/types.h> in "inode.h".
    
    Issue: #2
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- inode.h
+++ inode.h
@@ -3,7 +3,7 @@
 #ifndef BTRFS_FUSE_INODE_H
 #define BTRFS_FUSE_INODE_H
 
-#include <asm-generic/types.h>
+#include <linux/types.h>
 #include <sys/stat.h>
 #include "compat.h"
 #include "ondisk_format.h"
